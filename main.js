bd31ea02fa835cd6bc23558b450763ea107c45e9
jcp158 <jcp.158.8705@gmail.com>
2025-04-22 23:14:04 -0500

    dynamic offset fixed. Finally...

--- main.js
+++ main.js
@@ -7,3 +7,16 @@ for(var i = 0; i < aTags.length; i++){
 		window.location.replace(ele.getAttribute('data-href'));
 	});
 }
+
+function adjustPadding() {
+	const fixedDiv = document.querySelector('.Banner');
+	const contentDiv = document.querySelector('#body');
+
+	if(fixedDiv && contentDiv) {
+		const fixedDivHeight = fixedDiv.offsetHeight;
+		contentDiv.style.paddingTop = `${fixedDivHeight}px`;
+	}
+}
+
+adjustPadding();
+window.addEventListener('resize', adjustPadding);
